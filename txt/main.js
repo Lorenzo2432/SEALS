4d332ffce0307437977d356ae05d7e3211c9839c
Lorenzo2432 <51844069+Lorenzo2432@users.noreply.github.com>
2021-02-15 09:38:25 -0800

    Jr. SCLA website

---
+++ txt/main.js
@@ -0,0 +1,15 @@
+function myRespond(){
+    
+    let doublecheckfname1 = document.getElementById('txt').value;
+    document.getElementById('displaytxt').innerHTML= "txt:" + txt;
+    alert(firstname);
+
+    let doublechecklname1 = document.getElementById('txt2').value;
+    document.getElementById('displaytxt2').innerHTML= "txt2" + txt2;
+    alert(lastname);
+
+    let doublecheckpassword1 = document.getElementById('password').value;
+    document.getElementById('displaypass').innerHTML= "Passord:" +password;
+    alert(password);
+
+} 
