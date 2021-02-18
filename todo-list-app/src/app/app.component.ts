25b4f04028860c6200a96eb52cff4220b2905275
TXT-10 <txt-10@TXT-10s-MBP.lan1>
2021-02-18 15:32:36 -0800

    First commit

---
+++ todo-list-app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // Store data using this array
+  public todos = ['Finish Math', 'Buy Groceries', 'Guitar Lessons'];
+  // How will you take the input
+  // Add functions as you fit!
+  // How do add data to an array in js? hint: search .push()
+}
